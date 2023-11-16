8b3bd3f0aa74f8ba98b8dfeb71625dfb42425bf3
Felipe Razzini <feliperazzini95@gmail.com>
2023-11-16 17:33:24 +0100

    rails watch list

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -5,7 +5,7 @@ class BookmarksController < ApplicationController
   end
 
   def create
-    @list = list.find(params[:list_id])
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
     if @bookmark.save
